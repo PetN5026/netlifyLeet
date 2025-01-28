e0d4362e2daff0c40677b43852ef29156c764e8c
Peter <peterng.918@gmail.com>
2025-01-27 23:29:14 -0500

    added root route

--- functions/api.js
+++ functions/api.js
@@ -6,6 +6,10 @@ const api = express();
 const port = process.env.PORT || 3000;
 const router = Router();
 
+router.get("/", (req, res) => {
+  res.send({ res: "home" });
+});
+
 router.get("/hello", (req, res) => {
   res.send({ res: "hellow world" });
 });
