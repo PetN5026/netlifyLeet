a68635dcccba7ce4b2b7d48bd7deab27fde4303b
Peter <peterng.918@gmail.com>
2025-01-28 18:10:33 -0500

    added cors

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,8 +1,9 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
 import leetRoute from "./leetcode.js";
+import cors from "cors";
 const api = express();
-
+api.use(cors());
 const port = process.env.PORT || 3000;
 const router = Router();
 
